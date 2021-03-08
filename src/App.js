2fd53f71944cb7d32d180dfff7d29f10cf6fd44e
A <itthisakdr@gmail.com>
2021-03-08 21:45:59 +0700

    added jsx import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-// import styled from "@emotion/styled";
 
 function App() {
   const [data, setData] = useState({});
